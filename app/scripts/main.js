34b86bff55464c1a3ebf7ff3f4600de45f213e1a
thachmle <thachmle@aol.com>
2014-06-19 20:00:33 -0400

    completed code, functional

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,25 @@
 
-//testing to pull the elements into the html page//
-//testing to pull the title, and then move on with images...no luck so far
-//receving error undefine on html page as list item...it works but doesn't work..AH! SOS
-//test to pull just the title first and then attempt the image...error still..just added p element to test
-//$(document).ready(function() {
+//when everything is good to go then run
+$(document).ready(function() {
 
-var pull = '';
-etsyitems.forEach (function (a) {
-  pull =  '<li><img src="' + a.title + '" /><p>' + a.title + '</p>';
+var image, title, moneyCode, price;
 
-  $('#tracks').append(pull);
+//run through data and for each elements it finds...the items will be define by the last line
+//In this case jQuery will place all of them using .append in the targeted area ".final"
+//each element contain '' even if it's inside cource
+
+etsyitems.results.forEach(function(a) {
+     image = '<div class="images">' + '<img src="' + a.Images[0].url_570xN + '" style="width: 250px; height: 200px;"/></div>';
+     title = '<div class="title">' + a.title + '</div>';
+     moneyCode = '<div class="moneyCode">' + a.currency_code + '</div>';
+     price = '<div class="price">' + '$' + a.price + '</div>';
+
+//will place items in block title .final in html
+//will also add properties of image, title, moneyCode and price
+//this div will go into scss and won't show in html, don't mix this up
+     $('.final').append('<div class="etsy">' + image + title + moneyCode + price  + '</div>');
 });
 
 });
-
-
 
 
